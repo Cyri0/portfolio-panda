7c488a4899a02bba5ab580696221758002832b19
Nemes Tamás - CAD1 <nemestamas94@gmail.com>
2026-03-31 18:35:02 +0200

    Add contact form with styling and responsive layout

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,29 @@
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+
+      <h1>Vedd fel velem a kapcsolatot!</h1>
+
+      <form id="contactForm">
+        <div>
+          <label htmlFor="name">Név:</label>
+          <input type="text" id="name" /> <br/>
+
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" /> <br/>
+        </div>
+        
+        <div>
+          <label htmlFor="message">Üzenet:</label>
+          <textarea name="" id="message"></textarea>
+          <br/>
+          <button type="submit">Elküld</button>
+        </div>
+      </form>
+
+      <img src="https://randomfox.ca/images/82.jpg" 
+           style={{width: "100%"}} />
+    </div>
   )
 }
 
